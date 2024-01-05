eab0fed0e5d78d6865846f7d517246f6245f1a7a
Kasper Qvistgaard <Kasper.Qvistgaard@dgi.dk>
2024-01-05 12:59:02 +0100

    Carlist - Kasper

---
+++ Kasper/Car.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public record Car(string Make, string Model, int Year, int NumberOfTires, FuelType Fuel, string? LicensePlate)
+{
+	// Override ToString()
+	// Add new line after each car
+
+	        public override string ToString()
+        {
+            return $"{Make} {Model} from {Year} with {NumberOfTires} tires running on {Fuel}. License plate: {LicensePlate}";
+        }
+}		
