3b22cd1c768c4ba70fb44b96275d08905eddb1f3
Steen Mogensen <00stm@dgi.dk>
2024-01-05 10:29:44 +0100

    Added project. Added domain

---
+++ Domain/Models/Enums/FuelType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.Enums
+{
+    public enum FuelType
+    {
+        Petrol,
+        Diesel,
+        Electric
+    }
+}
